5332c31239cd36f0551fc81327c024aae4fbd832
jsalgua027 <jsalcedoguarde@gmail.com>
2023-04-24 13:18:16 +0200

    iniciado ejercicio 7E teminada clase factura

---
+++ ej7ENachoSalcedo/src/main/java/ej7enachosalcedo/Factura.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej7enachosalcedo;
+
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.stream.DoubleStream;
+import org.apache.commons.lang3.RandomStringUtils;
+
+/**
+ *
+ * @author nacho
+ */
+public class Factura {
+
+    private String codigoUnico;
+    private LocalDate fechaEmision;
+    private String descripcion;
+    private double totalImporteFactura;
+    private Random ale = new Random();
+
+    private static int contador = 1;
+
+    public Factura() {
+        this.codigoUnico = String.valueOf(contador++);
+        this.fechaEmision = LocalDate.now();
+        this.descripcion = RandomStringUtils.randomAlphabetic(10);
+        DoubleStream stream = ale.doubles(1, 100.0, 1000.0);
+        this.totalImporteFactura = stream.findFirst().getAsDouble();
+    }
+
+    public String getCodigoUnico() {
+        return codigoUnico;
+    }
+
+    public LocalDate getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getTotalImporteFactura() {
+        return totalImporteFactura;
+    }
+
+    public Random getAle() {
+        return ale;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public void setCodigoUnico(String codigoUnico) {
+        this.codigoUnico = codigoUnico;
+    }
+
+    public void setFechaEmision(LocalDate fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setTotalImporteFactura(double totalImporteFactura) {
+        this.totalImporteFactura = totalImporteFactura;
+    }
+
+    public void setAle(Random ale) {
+        this.ale = ale;
+    }
+
+    public static void setContador(int contador) {
+        Factura.contador = contador;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(codigoUnico);
+        sb.append(";").append(fechaEmision);
+        sb.append(";").append(descripcion);
+        sb.append(";").append(totalImporteFactura);
+
+        return sb.toString();
+    }
+
+}
