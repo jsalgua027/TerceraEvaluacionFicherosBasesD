a96fd4db88a1119c419bbcfaf512052fe4f018b8
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-05-25 21:06:04 +0200

    Iniciado modificaciones

--- musica/src/main/java/programa/Main.java
+++ musica/src/main/java/programa/Main.java
@@ -29,9 +29,9 @@ public class Main {
     private static final InstrumentoJpaController ic = new InstrumentoJpaController(emf);
     private static final GrabacionJpaController gc = new GrabacionJpaController(emf);
     private static final BiografiaJpaController bc = new BiografiaJpaController(emf);
-
+    
     public static void main(String[] args) throws NonexistentEntityException {
-
+        
         String menuPrincipal = """
                            
                                                      PRODUCCIONES MUSICALES GEPETO
@@ -46,7 +46,7 @@ public class Main {
                           
                           
                                             """;
-
+        
         String menuBiografia = """
                                                 
                                                     GESTION BIOGRAFIA
@@ -60,7 +60,7 @@ public class Main {
                           
                           
                                            """;
-
+        
         String menuMusicos = """
                                                 
                                                     GESTION MUSICOS
@@ -73,7 +73,7 @@ public class Main {
                                                 
                                                     
                                            """;
-
+        
         String menuInstrumentos = """
                           
                                                 
@@ -87,7 +87,7 @@ public class Main {
                                                 
                                                     
                                            """;
-
+        
         String menuGrabaciones = """
                        
                                                 
@@ -101,23 +101,45 @@ public class Main {
                                                 
                                                     
                                            """;
-
+        
         String menuCopiasSegurida = """
                                                             GESTION COPIAS DE SEGURIDAD
                                                              
                                                             1.Copia de seguridad de  listado de Biografias
                                                             2.Copia de seguridad de listado de Musicos
                                                             3.Copia de seguridad de listado de Instrumentos
-                                                            4.Copia de seguridad de listado de Granaciones
+                                                            4.Copia de seguridad de listado de Grabaciones
                                                             5. Salir
                                   
                                   
                                                            """;
-
+        
+        String modBio
+                = """
+                                  MODIFICACION BIOGRAFIAS
+                                 
+                                 1.Para modificar Datos
+                                 2.Para Añadir Musico
+                                 3.Salir
+                                 
+                                 
+                                 """;
+        
+        String modMusi
+                = """
+                                  MODIFICACION MUSICOs
+                                 
+                                 1.Para modificar Datos
+                                 2.Para Añadir Biografia
+                                 3.Salir
+                                 
+                                 
+                                 """;
+        
         String gestionMenu = " ";
-
+        
         Scanner entrada = new Scanner(System.in);
-
+        
         do {
             System.out.println(menuPrincipal);
             System.out.println("Elija una Opcion");
@@ -136,9 +158,9 @@ public class Main {
                                 int dia = 0;
                                 int mes = 0;
                                 int anio = 0;
-
+                                
                                 String lugarNacimientoAux;
-
+                                
                                 System.out.println("Gestion de Altas");
                                 System.out.println("Indique la descripcion de la Biografia");
                                 descriAux = entrada.nextLine();
@@ -153,24 +175,40 @@ public class Main {
                                     System.out.println("Que dia");
                                     dia = Utilidades.Utilidades.leerEnteroSinErroresScanner();
                                 } while (dia < 1 || dia > 31);
-
+                                
                                 System.out.println("Indique lugar de Nacimiento");
                                 lugarNacimientoAux = entrada.nextLine();
-
+                                
                                 fechaNaciAux = LocalDate.of(anio, mes, dia);
                                 altaBio.setDescripcion(descriAux);
                                 altaBio.setFechaNacimiento(Utilidades.Utilidades.LocalADate(fechaNaciAux));
                                 altaBio.setLugarNacimiento(lugarNacimientoAux);
                                 bc.create(altaBio);
                                 Utilidades.Utilidades.mostrarBiografia();
-
+                                
                                 break;
-
+                            
                             case "2":
-
+                                do {                                    
+                                    System.out.println(modBio);
+                                    System.out.println("Elija una Opcion");
+                                    gestionMenu = entrada.nextLine();
+                                    switch (gestionMenu) {
+                                        case "1":
+                                            System.out.println("Indique el codigo de biografia que quiere modificar");
+                                              Utilidades.Utilidades.mostrarBiografia();
+                                            
+                                            break;
+                                         case "2":
+                                            
+                                            break;
+                                       
+                                    }
+                                    
+                                } while (!gestionMenu.contains("3"));
                                 break;
                             case "3":
-
+                                
                                 break;
                             case "4":
                                 String codigoBorradoBio;
@@ -181,14 +219,14 @@ public class Main {
                                 bc.findBiografia(Integer.parseInt(codigoBorradoBio));
                                 bc.destroy(Integer.parseInt(codigoBorradoBio));
                                 Utilidades.Utilidades.mostrarBiografia();
-
+                                
                                 break;
-
+                            
                         }
-
+                        
                     } while (!gestionMenu.contains("5"));
                     break;
-
+                
                 case "2":
                     do {
                         System.out.println(menuMusicos);
@@ -196,23 +234,23 @@ public class Main {
                         gestionMenu = entrada.nextLine();
                         switch (gestionMenu) {
                             case "1":
-
+                                
                                 break;
-
+                            
                             case "2":
-
+                                
                                 break;
                             case "3":
-
+                                
                                 break;
                             case "4":
-
+                                
                                 break;
-
+                            
                         }
-
+                        
                     } while (!gestionMenu.contains("5"));
-
+                    
                     break;
                 case "3":
                     do {
@@ -221,21 +259,21 @@ public class Main {
                         gestionMenu = entrada.nextLine();
                         switch (gestionMenu) {
                             case "1":
-
+                                
                                 break;
-
+                            
                             case "2":
-
+                                
                                 break;
                             case "3":
-
+                                
                                 break;
                             case "4":
-
+                                
                                 break;
-
+                            
                         }
-
+                        
                     } while (!gestionMenu.contains("5"));
                     break;
                 case "4":
@@ -245,22 +283,22 @@ public class Main {
                         gestionMenu = entrada.nextLine();
                         switch (gestionMenu) {
                             case "1":
-
+                                
                                 break;
-
+                            
                             case "2":
-
+                                
                                 break;
                             case "3":
-
+                            
                             case "4":
-
+                                
                                 break;
-
+                            
                         }
-
+                        
                     } while (!gestionMenu.contains("5"));
-
+                    
                     break;
                 case "5":
                     do {
@@ -269,28 +307,28 @@ public class Main {
                         gestionMenu = entrada.nextLine();
                         switch (gestionMenu) {
                             case "1":
-
+                                
                                 break;
-
+                            
                             case "2":
-
+                                
                                 break;
                             case "3":
-
+                                
                                 break;
                             case "4":
-
+                                
                                 break;
-
+                            
                         }
-
+                        
                     } while (!gestionMenu.contains("5"));
-
+                    
                     break;
-
+                
             }
-
+            
         } while (!gestionMenu.contains("6"));
-
+        
     }
 }
